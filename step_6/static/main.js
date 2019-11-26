ae46e66aa26666695a4ad717b63a5d15917fc208
ZER0 <zer0.kaos@gmail.com>
2019-11-26 14:56:19 +0100

    bit of cleanup and js idiomatic approaches

--- step_6/static/main.js
+++ step_6/static/main.js
@@ -1,19 +1,19 @@
 import { init as initWasm } from "./wasm.js";
 import { init as initWebgl } from "./webgl.js";
-import { sierpinski as sierpinskiJs } from "./sierpinski.js";
+// import { sierpinski as sierpinskiJs } from "./sierpinski.js";
 
 export async function main() {
   let { sierpinski: sierpinskiWasm } = await initWasm(
     "/rust_workshop/target/wasm32-unknown-unknown/release/step_6_wasm.wasm"
   );
-  let now = Date.now();
+  let now = performance.now();
   let vertices = sierpinskiWasm(8);
-  console.log(Date.now() - now);
+  console.log(performance.now() - now);
   let canvas = document.getElementById("canvas");
   let gl = canvas.getContext("webgl");
   let { render } = initWebgl(gl, vertices);
-  requestAnimationFrame(function frame() {
-    render();
+  requestAnimationFrame(function frame(timestamp) {
+    render(timestamp);
     requestAnimationFrame(frame);
   });
 }
