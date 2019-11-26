2694ba4e4eb7d2da78c2cdf61b0c68ff7bb75d47
Eddy Bruel <ejpbruel@gmail.com>
2019-11-26 03:58:31 +0100

    Implement step_5

--- step_5/static/main.js
+++ step_5/static/main.js
@@ -4,10 +4,10 @@ import { sierpinski as sierpinskiJs } from "./sierpinski.js";
 
 export async function main() {
   let { sierpinski: sierpinskiWasm } = await initWasm(
-    "/rust_workshop/target/wasm32-unknown-unknown/release/step_1_wasm.wasm"
+    "/rust_workshop/target/wasm32-unknown-unknown/release/step_5_wasm.wasm"
   );
   let now = Date.now();
-  let vertices = sierpinskiWasm(8);
+  let vertices = sierpinskiWasm();
   console.log(Date.now() - now);
   let canvas = document.getElementById("canvas");
   let gl = canvas.getContext("webgl");
---
+++ step_6/src/lib.rs
@@ -0,0 +1,8 @@
+#![allow(dead_code)]
+
+#[macro_use]
+mod macros;
+
+mod math;
+mod sierpinski;
+mod wasm;
