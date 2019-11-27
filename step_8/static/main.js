260961ae605ca4bb82f49774a3dd0c294020e80a
ZER0 <zer0.kaos@gmail.com>
2019-11-27 23:17:50 +0100

    Add HighDPI support for canvas; refactor server node

--- step_8/static/main.js
+++ step_8/static/main.js
@@ -3,15 +3,21 @@ import { init as initWebgl } from "./webgl.js";
 // import { sierpinski as sierpinskiJs } from "./sierpinski.js";
 
 export async function main() {
-  await initWasm(
-    "./static/step_8_wasm_bg.wasm"
-  );
+  await initWasm("./static/step_8_wasm_bg.wasm");
   let now = performance.now();
   let vertices = sierpinskiWasm(8);
   console.log(performance.now() - now);
+
   let canvas = document.getElementById("canvas");
+
+  // Set the canvas' size based on device pixel ratio
+  const { width, height } = getComputedStyle(canvas);
+  canvas.width = parseFloat(width) * devicePixelRatio;
+  canvas.height = parseFloat(height) * devicePixelRatio;
+
   let gl = canvas.getContext("webgl");
   let { render } = initWebgl(gl, vertices);
+
   requestAnimationFrame(function frame(timestamp) {
     render(timestamp);
     requestAnimationFrame(frame);
